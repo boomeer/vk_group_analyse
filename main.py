d5488b07b7713415dbcec603e14b1c85eb8620f3
Dmitrii Bezrukov <di.bezrukov@gmail.com>
2015-10-25 22:23:42 +0300

    Update main.py
    code style

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ import numpy as np
 import networkx as nx
 from matplotlib import pyplot as plt
 
-
 PUBLIC = 1
 GROUP = 2
 OPEN = 1
@@ -28,10 +27,8 @@ TABLES = 'tables'
 SPLIT = 'split'
 TMP_TABLES = os.path.join(TABLES, 'tmp')
 
-
 def table(name, dir=TABLES):
     return os.path.join(dir, name + '.tsv')
-
 
 GROUPS_MEMBERS = table('groups_members')
 GROUP_COLUMN = 0
@@ -65,7 +62,6 @@ def download_groups_chunk(offset=0, type=PUBLIC):
     data = response.json()
     return data['response']['entity']
 
-
 def list_groups_chunks_cache(dir=GROUPS_CHUNKS):
     for filename in os.listdir(dir):
         match = re.match(r'(\d)_(\d+).json', filename)
@@ -74,24 +70,20 @@ def list_groups_chunks_cache(dir=GROUPS_CHUNKS):
         offset = int(offset)
         yield type, offset
 
-
 def groups_chunk_path(offset, type, dir=GROUPS_CHUNKS):
     filename = '{}_{}.json'.format(type, offset)
     path = os.path.join(dir, filename)
     return path
-
 
 def load_groups_chunk(offset, type, dir=GROUPS_CHUNKS):
     path = groups_chunk_path(offset, type, dir=dir)
     with open(path) as file:
         return cjson.decode(file.read())
 
-
 def dump_groups_chunk(chunk, offset, type, dir=GROUPS_CHUNKS):
     path = groups_chunk_path(offset, type, dir=dir)
     with open(path, 'w') as file:
         file.write(cjson.encode(chunk))
-
 
 def get_groups_chunk(offset=0, type=PUBLIC, dir=GROUPS_CHUNKS):
     cache = set(list_groups_chunks_cache(dir=dir))
@@ -102,23 +94,19 @@ def get_groups_chunk(offset=0, type=PUBLIC, dir=GROUPS_CHUNKS):
         dump_groups_chunk(chunk, offset, type, dir=dir)
         return chunk
 
-
 def read_groups_list(dir=GROUPS_CHUNKS):
     for type, offset in list_groups_chunks_cache(dir=dir):
         for record in load_groups_chunk(offset, type):
             yield record
 
-            
 def load_groups_list(dir=GROUPS_CHUNKS):
     data = pd.DataFrame(read_groups_list(dir=dir))
     data = data.sort('quantity', ascending=False, inplace=False)
     data.index = range(0, len(data))
     return data
 
-
 class ApiCallError(Exception):
     pass
-
 
 def call_vk(method, v=5.34, token=TOKEN, **params):
     params.update(v=v, access_token=token)
@@ -132,7 +120,6 @@ def call_vk(method, v=5.34, token=TOKEN, **params):
     else:
         return data['response']
 
-
 def download_members(id, offset=0, count=1000, fields=(), call_vk=call_vk):
     fields = ','.join(fields)
     return call_vk(
@@ -142,7 +129,6 @@ def download_members(id, offset=0, count=1000, fields=(), call_vk=call_vk):
         count=count,
         fields=fields
     )
-
 
 def list_members_cache(dir=MEMBERS):
     for hash in range(10):
@@ -155,11 +141,9 @@ def list_members_cache(dir=MEMBERS):
                 offset = int(offset)
                 yield id, offset
 
-
 def members_path(id, offset, dir=MEMBERS):
     path = os.path.join(dir, str(id % 10), str(id), str(offset) + '.json')
     return path
-
 
 def load_members(id, offset, dir=MEMBERS):
     path = members_path(id, offset, dir=dir)
@@ -167,7 +151,6 @@ def load_members(id, offset, dir=MEMBERS):
         banned, members = cjson.decode(file.read())
         banned = set(banned)
         return banned, members
-
 
 def dump_members(members, id, offset, dir=MEMBERS):
     path = members_path(id, offset, dir=dir)
@@ -178,21 +161,17 @@ def dump_members(members, id, offset, dir=MEMBERS):
     with open(path, 'w') as file:
         file.write(cjson.encode(members))
 
-
 def in_vkscript(method, **params):
     return 'API.{}({})'.format(method, cjson.encode(params))
 
-
 def call_for(function, *args, **kwargs):
     return function(*args, call_vk=in_vkscript, **kwargs)
-
 
 def run_execute(code):
     return call_vk(
         'execute',
         code=code
     )
-
 
 def run_bulk(calls):
     calls = list(calls)
@@ -202,9 +181,7 @@ def run_bulk(calls):
     code = 'return [{}];'.format(','.join(calls))
     return run_execute(code)
 
-
 MembersFuture = namedtuple('MembersFuture', 'function, id, offset, count, fields')
-
 
 def generate_download_members_calls(groups):
     for _, row in groups.iterrows():
@@ -215,11 +192,9 @@ def generate_download_members_calls(groups):
                 download_members, id, offset, 1000, ('deactivated',)
             )
 
-
 def group_download_members_calls(calls, size=25):
     for _, group in groupby(enumerate(calls), lambda (index, call): index / size):
         yield [call for _, call in group]
-
 
 def download_members_bulks(groups, dir=MEMBERS):
     cache = set(list_members_cache(dir=dir))
@@ -249,7 +224,6 @@ def download_members_bulks(groups, dir=MEMBERS):
                 members = (banned, users)
                 dump_members(members, call.id, call.offset, dir=dir)
 
-
 def get_members_download_progress(dir=MEMBERS):
     data = []
     for id, offset in list_members_cache(dir=dir):
@@ -260,14 +234,12 @@ def get_members_download_progress(dir=MEMBERS):
     data.time = pd.to_datetime(data.time, unit='s')
     return data
 
-
 def show_progress(progress):
     table = progress.groupby('id').time.max()
     table = table.sort(inplace=False)
     table = table.reset_index().set_index('time')
     table['groups'] = 1
     table.groups.cumsum().plot()
-
 
 def read_groups_members(dir=MEMBERS, ignore_banned=True):
     cache = list_members_cache(dir=dir)
@@ -277,27 +249,22 @@ def read_groups_members(dir=MEMBERS, ignore_banned=True):
             if not ignore_banned or index not in banned:
                 yield id, member
 
-
 def serialize_groups_members(stream):
     for id, member in stream:
         yield str(id), str(member)
 
-
 def deserialize_groups_members(stream):
     for id, member in stream:
         yield int(id), int(member)
-
 
 def read_table(table):
     with open(table) as file:
         for line in file:
             yield line.rstrip('\n').split('\t')
 
-
 def write_table(stream, table):
     with open(table, 'w') as file:
         file.writelines('\t'.join(_) + '\n' for _ in stream)
-
 
 def sort_table(table, by, chunks=20):
     if not isinstance(by, (list, tuple)):
@@ -329,29 +296,24 @@ def sort_table(table, by, chunks=20):
             path = os.path.join(TMP_TABLES, name)
             os.remove(path)
 
-
 def group_stream(stream, by):
     if isinstance(by, (list, tuple)):
         return groupby(stream, lambda r: [r[_] for _ in by])
     else:
         return groupby(stream, lambda r: r[by])
 
-
 def stream_size(stream):
     return sum(1 for _ in stream)
-
 
 def reduce_user_groups(groups):
     for user, group in groups:
         count = stream_size(group)
         yield user, count
 
-
 def reduce_groups_sizes(groups):
     for group, records in groups:
         size = stream_size(records)
         yield group, size
-
 
 def log_progress(stream, every=1000, total=None):
     if total:
@@ -366,12 +328,10 @@ def log_progress(stream, every=1000, total=None):
             print >>sys.stderr, progress,
         yield record
 
-
 def get_table_size(table):
     output = subprocess.check_output(['wc', '-l', table])
     size, _ = output.split(None, 1)
     return int(size)
-
 
 def reduce_combinations(groups, cap=10):
     for user, records in groups:
@@ -380,16 +340,13 @@ def reduce_combinations(groups, cap=10):
             for source, target in combinations(groups, 2):
                 yield source, target
 
-
 def serialize_combinations(stream):
     for source, target in stream:
         yield str(source), str(target)
 
-
 def deserialize_combinations(stream):
     for source, target in stream:
         yield int(source), int(target)
-
 
 def reduce_edges(groups, sizes):
     for (source, target), records in groups:
@@ -397,13 +354,11 @@ def reduce_edges(groups, sizes):
         yield source, target, float(intersection) / sizes[source]
         yield target, source, float(intersection) / sizes[target]
 
-
 def prepare_name(name):
     name = name.lower()
     words = re.findall(r'(\w+)', name, re.U)
     name = ' '.join(words)
     return name
-
 
 def build_graph(edges, sizes, names):
     nodes = set()
@@ -422,10 +377,8 @@ def build_graph(edges, sizes, names):
         graph.add_edge(source, target, weight=weight)
     return graph
 
-
 def save_graph(graph, path='graph.gexf'):
     nx.write_gexf(graph, path)
-
 
 def build_data(edges, sizes, names, top=1000, cap=10):
     nodes = defaultdict(Counter)
@@ -454,7 +407,6 @@ def save_data(data, path='viz/data.json'):
 
     with open(path, 'w') as file:
         json.dump(data, file)
-
 
 def show_related(id, edges, names, cap=10, html=False):
     related = Counter()
